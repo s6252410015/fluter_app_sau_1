61c5be05c3c23039471cb08ab2628347e3050301
s6252410015 <ss6252410015@sau.ac.th>
2021-08-27 10:27:43 +0700

    Version 1

---
+++ lib/views/first_ui.dart
@@ -0,0 +1,51 @@
+
+import 'package:flutter/material.dart';
+
+class FirstUI extends StatelessWidget {
+  const FirstUI({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar:AppBar(
+        backgroundColor: Colors.pink[100],
+        title:Text(
+          'FirstUI ^_^ ' ,
+        ),
+        centerTitle: true ,
+      ) ,
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.pinkAccent[100],
+        onPressed: (){},
+        child: Icon(
+          Icons.add,
+          color:Colors.white,
+        ),
+      ),    
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat ,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'กฤศ ศรีฤกษ์',
+              style: TextStyle(
+                fontSize: 25.0 ,
+                fontWeight : FontWeight.bold,
+                color: Colors.blue[300],
+              ),
+            ),
+            Text(
+              '6252410015',
+              style: TextStyle(
+                fontSize: 25.0 ,
+                fontWeight : FontWeight.bold,
+                color: Colors.blue[200],
+              ),
+            ),
+          ],
+        ),
+      )
+    );
+  }
+}
